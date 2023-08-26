03134b575fa01c2584897e147a4aa571a142cb78
sandeepkuraria <sandeep.kuraria@gmail.com>
2023-08-27 02:38:27 +0530

    Added some message indication feature to recognise what is wrong in given paragraph in textMatching.js file also in index.html file for Paragraph Matching Assignment.

--- Assgnment13JS/textMatching.js
+++ Assgnment13JS/textMatching.js
@@ -1,19 +1,34 @@
 function textMatching() {
-  document.getElementById("para1").innerHTML = m;
-  document.getElementById("para2").innerHTML = n;
+  const m = document.getElementById("para1").textContent;
+
+  const nContent = document.getElementById("para2");
+
+  const n = prompt("Enter the para2");
+
   let result = "";
 
   for (let i = 0; i < Math.max(m.length, n.length); i++) {
     if (m[i] === n[i]) {
       result += m[i];
     } else {
-      // result = document.getElementById("para2").innerHTML = "";
+      if (m.length !== n.length) {
+        document.getElementById("message").innerHTML = message1;
+      }
+      if (n[0] === " ") {
+        document.getElementById("message").innerHTML = message2;
+      }
+      if (m[i] !== n[i] && m.length === n.length) {
+        document.getElementById("message").innerHTML =
+          "wrong character in the paragraph.";
+      }
       result += `<span style='color: red;'>${n[i] || " "}</span>`;
-
-      //   document.getElementById("para2").style.color = "red";
     }
   }
-  document.getElementById("para2").innerHTML = result;
+
+  nContent.innerHTML = result;
 }
-const m = prompt("Enter the para1");
-const n = prompt("Enter the para2");
+
+const message1 =
+  "Maybe you are missing some characters or adding extra characters or words in the paragraph.";
+const message2 =
+  "You should not start the matching paragraph with empty string or space.";
